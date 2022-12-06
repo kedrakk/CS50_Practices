c0c5c9a477f4510eb5bb8423c7c3a329905b09bc
kedrakk <keaindrak@gmail.com>
2022-12-06 22:28:16 +0630

    add linear search

--- c/hello.c
+++ c/hello.c
@@ -1,6 +1,5 @@
-#include <cs50.h>
 #include <stdio.h>
 
 int main(void){
-    printf("Hello, World!");
+    printf("Hello, World!\n");
 }
---
+++ c/linear_search.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int linear_search(int intArray[],int numberToSearch,int arraySize);
+
+int main(void){
+    int intArray[6]={4,2,6,1,5,3};
+    int numberToSearch=0;
+    int arraySize=sizeof(intArray)/sizeof(intArray[0]);
+    int searchOne=linear_search(intArray,numberToSearch,arraySize);
+    if(searchOne!=-1){
+        printf("Found %i\n",numberToSearch);
+    }else{
+        printf("Not Found!\n");
+    }
+}
+
+int linear_search(int arrayToSearch[],int numberToSearch,int arraySize){
+    for (int i = 0; i < arraySize; i++)
+    {
+        if(arrayToSearch[i]==numberToSearch){
+            return 1;
+        }
+    }
+    return -1;
+}
