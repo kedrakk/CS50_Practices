1828fdfbc71b4b4751b6c3cc2de78a3367c0aaab
kedrakk <keaindrak@gmail.com>
2022-12-07 21:40:31 +0630

    recursion

---
+++ c/recursion.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+void draw(int count);
+
+int main(void){
+    int count=5;
+    draw(count);
+}
+
+void draw(int count){
+    if(count<=0){
+        return;
+    }
+    draw(count-1);
+    for(int i=0;i<count;i++)
+    {
+        printf("*");
+    }
+    printf("\n");
+    
+}
